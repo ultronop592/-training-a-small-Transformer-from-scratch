001e0bd208ad33e2aa6587a250576e7b2768d401
ultronop492 <ultronop558@gmail.com>
2026-07-10 23:54:36 +0530

    fixed the train_inabs file

--- config/train_inabs.py
+++ config/train_inabs.py
@@ -1,37 +1,27 @@
-# model 
- 
-out_dir = 'out_inabs'
-eval_interval = 500    
-log_interval = 10
-eval_iters = 50
- 
- 
-dataset = 'inabs'
- 
-batch_size = 4
+out_dir    = 'out-inabs'
+dataset    = 'inabs'
+
+eval_interval = 500
+log_interval  = 10
+eval_iters    = 50
+
+batch_size                  = 4
 gradient_accumulation_steps = 8
- 
-vocal_size = 8000
-d_model = 256
-n_heads = 4
-d_ff = 512
-n_encoder_layers = 3
-n_decoder_layers = 3
-dropout = 0.1
 
+vocab_size   = 8000
+d_model      = 256
+n_heads      = 4
+d_ff         = 512
+n_enc_layers = 3
+n_dec_layers = 3
+dropout      = 0.1
+src_max_len  = 1024
+tgt_max_len  = 256
 
-
-src_max_len = 1024
-tgt_max_len = 256 
-
-learning_rate = 3e-4
-max_iter = 10000
-weight_decay = 1e-1
-
-
-warmup_iters = 400 
-lr_deacy_iters = 10000
-min_lr = 3e-5
-
-
-compile = False 
+learning_rate  = 3e-4
+max_iters      = 10000
+weight_decay   = 1e-1
+warmup_iters   = 400
+lr_decay_iters = 10000
+min_lr         = 3e-5
+compile        = False
